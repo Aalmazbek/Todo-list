2326e9dff72c32899a15f36bb11915f133b679b0
Алмаз Ишенбеков <ishenbekovalmaz@gmail.com>
2023-03-20 18:57:53 +0600

    checkSpaces deleted

--- src/components/createTodo/CreateTodo.jsx
+++ src/components/createTodo/CreateTodo.jsx
@@ -1,18 +1,7 @@
 import css from './CreateTodo.module.css'
 
-let checkSpaces = str => str.trim() != ""
-
 
 const CreateTodo = (props) => {
-
-    function handleClick(e){
-        if(checkSpaces(e.target.previousElementSibling.value)){
-            console.log(e.target.previousElementSibling.value);
-            e.target.previousElementSibling.value = ''
-        }
-    }
-
-    
     
     return (
         <div className={css.CreateTodo}>
